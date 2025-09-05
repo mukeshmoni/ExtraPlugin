89c8f2d7e5036cfb2f6634c14cbf89fe60262d23
MUKESH BABU S <108213299+mukeshmoni@users.noreply.github.com>
2025-09-05 18:02:07 +0530

    Update chatgpt.py

--- plugins/chatgpt.py
+++ plugins/chatgpt.py
@@ -6,6 +6,35 @@ from MOONMUSIC import app
 from config import BANNED_USERS
 
 
+@app.on_message(filters.command(["detect", "aidetect", "asklang"]) & ~BANNED_USERS)
+async def chatgpt_chat_lang(bot, message):
+    if len(message.command) < 2 and not message.reply_to_message:
+        await message.reply_text("**Provide any text after command or reply to any message**")
+            
+        return
+
+    if message.reply_to_message and message.reply_to_message.text:
+        user_text = message.reply_to_message.text
+    else:
+        user_text = " ".join(message.command[1:])
+
+    user_input = f"""
+    Sentences :- {user_text}
+    Ye sentence kon sa language me hai mujhe bas ush sentences ka lang name aur lang code, sath me ush sentence ka chatbot jaisa chhota se chhota reply do jyada bada reply mat dena maximum 1 sentence ka hona chahiye reply aur agar chhota se chhota reply me bhi kam ho ja rha hai to chhota hi reply do aur jis lang me sentence hai usi lang me likh ke do, agar sentence me sirf emoji hoga to tum bhi bas emoji dena aur ish format me likh ke do :- 
+
+    Lang : -
+    Code :-
+    Reply :- 
+
+    Bas lang name aur lang code aur reply likh ke do uske alava kuch nhi
+    """
+
+    # Send typing action and get the response from API
+    await bot.send_chat_action(message.chat.id, ChatAction.TYPING)
+    results = api.chatgpt(user_input)
+    await message.reply_text(results)
+
+
 @app.on_message(filters.command(["chatgpt", "ai", "ask"]) & ~BANNED_USERS)
 async def chatgpt_chat(bot, message):
     if len(message.command) < 2 and not message.reply_to_message:
@@ -24,7 +53,7 @@ async def chatgpt_chat(bot, message):
     await message.reply_text(results)
 
 
-__MODULE__ = "🌙 𝐂𖽻𖽖𖾓ɢ𖽳𖾓 🫧"
+__MODULE__ = "CʜᴀᴛGᴘᴛ"
 __HELP__ = """
 /advice - ɢᴇᴛ ʀᴀɴᴅᴏᴍ ᴀᴅᴠɪᴄᴇ ʙʏ ʙᴏᴛ
 /ai [ǫᴜᴇʀʏ] - ᴀsᴋ ʏᴏᴜʀ ǫᴜᴇsᴛɪᴏɴ ᴡɪᴛʜ ᴄʜᴀᴛɢᴘᴛ's ᴀɪ
