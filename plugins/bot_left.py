5c1409ae2b36141d25f0bd9b2225aeed1860dcc1
MUKESH BABU S <108213299+mukeshmoni@users.noreply.github.com>
2025-09-05 17:49:34 +0530

    Update bot_left.py

--- plugins/bot_left.py
+++ plugins/bot_left.py
@@ -35,14 +35,14 @@ async def on_left_chat_member(_, message: Message):
         left_chat_member = message.left_chat_member
         if left_chat_member and left_chat_member.id == (await app.get_me()).id:
             remove_by = (
-                message.from_user.mention if message.from_user else "🌙 𝐔𖽪𝙺𖽡𖽙𖽮 𝐔𖾗𖽞𖽷 🫧"
+               message.from_user.mention if message.from_user else "𝐔ɴᴋɴᴏᴡɴ 𝐔sᴇʀ"
             )
             title = message.chat.title
             username = (
-                f"@{message.chat.username}" if message.chat.username else "🌙 𝐏𖽷𖽹ᵥ𖽖𖾓𖽞  𝐂𖽻𖽖𖾓 🫧"
+                f"@{message.chat.username}" if message.chat.username else "𝐏ʀɪᴠᴀᴛᴇ 𝐂ʜᴀᴛ"
             )
             chat_id = message.chat.id
-            left = f"✫ <b><u>🌙 𝐋𖽞ꜰ𖾓 𝐆𖽷𖽙𖽪𖽳 🫧</u></b> ✫\n\n🌙 𝐂𖽻𖽖𖾓 𝐓𖽹𖾓𖾘𖾔 🫧  {title}\n\n🌙 𝐂𖽻𖽖𖾓  𝐈𖽴 🫧  {chat_id}\n\n🌙 𝐑𖽞𖽧𖽙ᵥ𖾝 𝐁ʏ 🫧 {remove_by}\n\n🌙 𝐁𖽙𖾓  🫧 @{app.username}"
+            left = f"✫ <b><u>#𝐋ᴇғᴛ_𝐆ʀᴏᴜᴘ</u></b> ✫\n\n𝐂ʜᴀᴛ 𝐓ɪᴛʟᴇ : {title}\n\n𝐂ʜᴀᴛ 𝐈ᴅ : {chat_id}\n\n𝐑ᴇᴍᴏᴠᴇᴅ 𝐁ʏ : {remove_by}\n\n𝐁ᴏᴛ : @{app.username}"
             await app.send_photo(LOG_GROUP_ID, photo=random.choice(photo), caption=left)
             await delete_served_chat(chat_id)
             await VIP.st_stream(chat_id)
