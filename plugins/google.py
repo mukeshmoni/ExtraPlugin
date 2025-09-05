3831aee2cb6d0abcc39f42c458aba2b22f302e47
MUKESH BABU S <108213299+mukeshmoni@users.noreply.github.com>
2025-09-05 18:33:08 +0530

    Update google.py

--- plugins/google.py
+++ plugins/google.py
@@ -59,6 +59,6 @@ async def app(bot, message):
         await message.reply_text(e)
 
 
-__MODULE__ = "🌙 𝐆𖽙𖽙ɢ𖾘𖾝 🫧"
+__MODULE__ = "Gᴏᴏɢʟᴇ"
 __HELP__ = """/google [ǫᴜᴇʀʏ] - ᴛᴏ sᴇᴀʀᴄʜ ᴏɴ ɢᴏᴏɢʟᴇ ᴀɴᴅ ɢᴇᴛ ʀᴇsᴜʟᴛs
 /app | /apps [ᴀᴘᴘ ɴᴀᴍᴇ] - ᴛᴏ ɢᴇᴛ ᴀᴘᴘ ɪɴғᴏ ᴛʜᴀᴛ ᴀᴠᴀɪʟᴀʙʟᴇ ᴏɴ ᴘʟᴀʏsᴛᴏʀᴇ"""
